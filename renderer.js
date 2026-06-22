61d0bd88dd5ac4f5d4a552b2a720989ad4bf24f8
tronzard <satyamkhatiwada1999@gmail.com>
2026-06-22 12:07:07 +0545

    animation changes on phase change

--- renderer.js
+++ renderer.js
@@ -1,6 +1,6 @@
-const FOCUS_MIN = 1,
+const FOCUS_MIN = 20,
   FOCUS_MAX = 60,
-  FOCUS_STEP = 1;
+  FOCUS_STEP = 5;
 const BREAK_MIN = 5,
   BREAK_MAX = 20,
   BREAK_STEP = 1;
@@ -10,7 +10,7 @@ const ICON_PLAY = `<svg width="10" height="11" viewBox="0 0 10 11" fill="none" x
 const ICON_PAUSE = `<svg width="10" height="11" viewBox="0 0 10 11" fill="none" xmlns="http://www.w3.org/2000/svg"><rect x="1" y="1" width="3" height="9" rx="1" fill="white"/><rect x="6" y="1" width="3" height="9" rx="1" fill="white"/></svg>`;
 
 //  state
-let focusMin = 25;
+let focusMin = 0;
 let breakMin = 5;
 let phase = "focus";
 let running = false;
@@ -74,6 +74,16 @@ function nextPhase() {
   alarm.play();
   renderTimer();
   renderPhase();
+
+  // flash + shake the tomato
+  tomato.classList.remove("phase-alert");
+  void tomato.offsetWidth;
+  tomato.classList.add("phase-alert");
+  tomato.addEventListener(
+    "animationend",
+    () => tomato.classList.remove("phase-alert"),
+    { once: true },
+  );
 }
 
 function startInterval() {
